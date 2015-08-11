eea663a7294d107d4847294f7040b5771263b4e1
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-08-11 13:15:57 +0200

    feature(grunt): automate releases

--- start.php
+++ start.php
@@ -3,9 +3,12 @@
 /**
  * Prototyper UI
  *
- * @author Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
+ * @author Ismayil Khayredinov <info@hypejunction.com>
  */
-require_once dirname(dirname(dirname(__FILE__))) . '/vendor/autoload.php';
+$path = __DIR__;
+if (file_exists("{$path}vendor/autoload.php")) {
+	require_once "{$path}vendor/autoload.php";
+}
 
 elgg_register_event_handler('init', 'system', 'prototyper_ui_init');
 
@@ -22,7 +25,7 @@ function prototyper_ui_init() {
 		elgg_register_js('prototyper_ui', elgg_get_simplecache_url('js', 'framework/legacy/prototyper_ui'), 'footer');
 
 		elgg_register_js('jquery.cropper', '/mod/hypePrototyperUI/vendors/jquery.cropper/cropper.min.js', 'footer');
-		
+
 		elgg_register_simplecache_view('js/framework/legacy/prototyper_cropper');
 		elgg_register_js('prototyper_cropper', elgg_get_simplecache_url('js', 'framework/legacy/prototyper_cropper'), 'footer');
 	} else {
@@ -33,6 +36,6 @@ function prototyper_ui_init() {
 	}
 
 	elgg_register_css('jquery.cropper', '/mod/hypePrototyperUI/vendors/jquery.cropper/cropper.min.css');
-	
+
 	elgg_extend_view('input/file', 'prototyper/ui/cropper');
 }
